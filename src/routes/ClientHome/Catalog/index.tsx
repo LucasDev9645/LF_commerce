bdf4630caa0bc645372c4696a0685f568cc6e8b2
LucasDev <LucasDev9645@gmail.com>
2023-05-04 21:39:22 -0300

    renderizando os produtos de forma statica

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -2,31 +2,9 @@ import SearchBar from "../../../components/SearchBar";
 import CatalogCard from "../../../components/CatalogCard";
 import ButtonNextPage from "../../../components/ButtonNextPage";
 import { ProductDTO } from "../../../models/product";
+import * as productService from "../../../services/product-service";
 
 import "./styles.css";
-
-const product: ProductDTO = {
-  id: 2,
-  name: "Pc Gamer",
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-  price: 5190.0,
-  imgUrl: "https://m.media-amazon.com/images/I/610eucL6jjL._AC_.jpg",
-  categories: [
-    {
-      id: 2,
-      name: "Eletrônicos",
-    },
-    {
-      id: 3,
-      name: "Computadores Gamer",
-    },
-    {
-      id: 4,
-      name: "Premiun",
-    },
-  ],
-};
 
 const Catalog = () => {
   return (
@@ -34,15 +12,9 @@ const Catalog = () => {
       <section id="catalog-section" className="dsc-container">
         <SearchBar />
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
-          <CatalogCard product={product} />
+          {productService.findAll().map((product) => (
+            <CatalogCard key={product.id} product={product} />
+          ))}
         </div>
         <ButtonNextPage />
       </section>
