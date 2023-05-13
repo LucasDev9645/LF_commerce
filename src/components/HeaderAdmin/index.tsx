8d06da12bb0857cc6fc2ee7dc806f486e94c3d41
LucasDev <LucasDev9645@gmail.com>
2023-05-13 16:21:36 -0300

    Admin navigation bar customization

--- src/components/HeaderAdmin/index.tsx
+++ src/components/HeaderAdmin/index.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import homeIcon from "../../assets/img/home-svgrepo-com 1.svg";
 import stockIcon from "../../assets/img/stock-svgrepo-com 1.svg";
 import LoggedUser from "../LoggedUser";
@@ -9,17 +10,31 @@ const HeaderAdmin = () => {
     <div>
       <header className="dsc-header-admin">
         <nav className="dsc-container">
-          <h1>LFC Admin</h1>
+          <h1>LFcommerce</h1>
           <div className="dsc-navbar-right">
             <div className="dsc-menu-items-container">
-              <div className="dsc-menu-item">
-                <img src={homeIcon} alt="Início" />
-                <p>Início</p>
-              </div>
-              <div className="dsc-menu-item">
-                <img src={stockIcon} alt="Cadastro de produtos" />
-                <p className="dsc-menu-item-active">Produtos</p>
-              </div>
+              <NavLink
+                to="/admin/home"
+                className={({ isActive }) =>
+                  isActive ? "dsc-menu-item-active" : ""
+                }
+              >
+                <div className="dsc-menu-item">
+                  <img src={homeIcon} alt="Início" />
+                  <p>Início</p>
+                </div>
+              </NavLink>
+              <NavLink
+                to="/admin/products"
+                className={({ isActive }) =>
+                  isActive ? "dsc-menu-item-active" : ""
+                }
+              >
+                <div className="dsc-menu-item">
+                  <img src={stockIcon} alt="Cadastro de produtos" />
+                  <p>Produtos</p>
+                </div>
+              </NavLink>
             </div>
             <LoggedUser />
           </div>
